8e0a5759da2889f06dbdedc4502d227253d3d54e
RTnhN <43109787+RTnhN@users.noreply.github.com>
2021-05-28 01:55:28 -0500

    Added Captcha to webapp Added logging to webapp Moved flask config to config.py file Isolated webapp only config to local only config file

--- main.py
+++ main.py
@@ -18,22 +18,31 @@
 # set FLASK_APP=main.py 
 # set FLASK_ENV=development this turns on debug mode that updates the server instance each time the code is updated
 # flask run or python -m flask run
+# To launch the web app:
+# gcloud app deploy app.yaml
 # [START app]
 import os
 from flask import Flask, jsonify, render_template,redirect,flash, request, url_for, send_from_directory
 from werkzeug.utils import secure_filename
 from algorithm import algorithm
+import requests
+import logging
+import google.cloud.logging
+client = google.cloud.logging.Client()
+client.get_default_handler()
+client.setup_logging()
 
-WEB_APP = False
-if WEB_APP: 
-    UPLOAD_FOLDER = "/tmp"
-else:
-    UPLOAD_FOLDER = "tmp"
-
+VERIFY_URL = "https://hcaptcha.com/siteverify"
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
-app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['MAX_CONTENT_LENGTH'] = 7 * 1024 * 1024 # 7MB limit for image upload
+app = Flask(__name__, instance_relative_config=True)
+
+WEBAPP = True
+
+if WEBAPP:
+    app.config.from_pyfile("configWebApp.py")
+else:
+    app.config.from_pyfile("config.py")
+
 
 def allowed_file(filename):
     return '.' in filename and \
@@ -49,23 +58,28 @@ def startingpoint():
     fileError = ""
     scroll = ""
     if request.method =="POST":
-        if 'file' not in request.files:
-            fileError = "<p>Please upload a file before hitting Submit</p>"
+        if not app.config['RECAPTCHA_ENABLED'] or verify() :
+            if 'file' not in request.files:
+                fileError = "<p>Please upload a file before hitting Submit</p>"
+                scroll = "Live-App"
+            file = request.files['file']
+            if file.filename == '':
+                fileError = "<p>Please upload a file before hitting Submit</p>"
+                scroll = "Live-App"
+            if not allowed_file(file.filename):
+                fileError = "<p>Please upload a file with only one of the allowed types (.png, .jpg, .jpeg, .gif)</p>"
+                scroll = "Live-App"    
+            if file and allowed_file(file.filename):
+                filename = secure_filename(file.filename)
+                file.save(app.config['UPLOAD_FOLDER'] + "/" +filename)
+                return redirect(url_for('processed',
+                                        filename=filename))
+        else:
+            fileError = "<p>There was a problem with the captcha, please try again</p>"
             scroll = "Live-App"
-        file = request.files['file']
-        if file.filename == '':
-            fileError = "<p>Please upload a file before hitting Submit</p>"
-            scroll = "Live-App"
-        if not allowed_file(file.filename):
-            fileError = "<p>Please upload a file with only one of the allowed types (.png, .jpg, .jpeg, .gif)</p>"
-            scroll = "Live-App"    
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(app.config['UPLOAD_FOLDER'] + "/" +filename)
-            return redirect(url_for('processed',
-                                    filename=filename))
         
-    name1 = fileError + '''<form method=post enctype=multipart/form-data> 
+    name1 = fileError + '''<form method=post enctype=multipart/form-data>
+              <div class="h-captcha" data-sitekey="2380b2b1-0209-4631-8c8f-1432f6528777"></div>
               <input type=file name=file> 
               <input type=submit value=Upload>
               </form>'''
@@ -92,4 +106,19 @@ def processed(filename):
 
     '''
     return render_template("template.html", my_string = html_snippet, scroll="Live-App")   
+
+def verify():
+    data = {
+        "secret": app.config['RECAPTCHA_SECRET_KEY'],
+        "response": request.form.get('h-captcha-response'),
+        "remoteip": request.environ.get('REMOTE_ADDR')
+    }
+
+    r = requests.post(VERIFY_URL, data=data)
+
+    logging.info("This is the responce from hcaptcha: "+str(r))
+    logging.info("This is the json: "+ str(r.json()))
+    return r.json()["success"] if r.status_code == 200 else False
+
+
 
